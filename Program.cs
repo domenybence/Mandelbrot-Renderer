f8701a0faf26b4e242c40eae83bebb3f9c48859c
domenybence <13c-domeny@ipari.vein.hu>
2025-07-18 16:21:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mandelbrot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
