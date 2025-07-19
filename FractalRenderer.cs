c4c166605778cab01de1d09070bfc2ba890cf3ad
domenybence <13c-domeny@ipari.vein.hu>
2025-07-19 15:03:27 +0200

    Implemented UV rendering and others.

---
+++ FractalRenderer.cs
@@ -0,0 +1,60 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp.Drawing.Processing;
+
+namespace Mandelbrot {
+    internal class FractalRenderer {
+        private const double GLOBAL_WIDTH = 3;
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Complex Center { get; set; }
+        public double Zoom { get; set; }
+        public int Iterations { get; set; }
+
+        public FractalRenderer(int width, int height, Complex center, double zoom, int iterations) {
+            Width = width;
+            Height = height;
+            Center = center;
+            Zoom = zoom;
+            Iterations = iterations;
+        }
+
+        public Image<Rgb24> Render() {
+            Rgb24[] pixels = new Rgb24[Width * Height];
+
+            for (int y = 0; y < Height; y++) {
+                for (int x = 0; x < Width; x++) {
+                    int pixelId = y * Width + x;
+
+                    double u = (double)x * 2 / Width - 1;
+                    double v = (double)y * 2 / Height - 1;
+
+                    byte r = (byte)Math.Clamp(u * 255,0,255);
+                    byte g = (byte)Math.Clamp(v * 255,0,255);
+                    byte b = 0;
+
+                    pixels[pixelId] = new Rgb24(r, g, b);
+                }
+            }
+            return Image.LoadPixelData<Rgb24>(pixels, Width, Height);
+        }
+
+        private int MandelbrotValue(Complex c) {
+            Complex z = Complex.Zero;
+            int i = 0;
+            for ( ; i < Iterations; i++) {
+                z = z * z + c;
+                if (z.Real * z.Real + z.Imaginary * z.Imaginary > 4) break;
+            }
+            return i;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Mandelbrot
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace Mandelbrot
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //center of cam - complex
+            //camera width - int
+            //camera height - int
+            //camera zoom - double
+            //camera width(3) in coordinate system
+
+            Complex center = new Complex(0, 0);
+            int width = 800;
+            int height = 600;
+            double zoom = 1;
+            int iterations = 20;
+
+            FractalRenderer renderer = new FractalRenderer(width, height, center, zoom, iterations);
+            Image<Rgb24> image = renderer.Render();
+
+            image.Save("kep.png");
+
+            Process.Start(new ProcessStartInfo("kep.png") { UseShellExecute = true });
         }
     }
 }
